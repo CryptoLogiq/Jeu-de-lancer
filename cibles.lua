d51de1956977af7061d748148c65671c5cdf7da6
Kifyu56 <70178350+Kifyu56@users.noreply.github.com>
2020-11-24 21:12:41 +0100

    Add files via upload

---
+++ cibles.lua
@@ -0,0 +1,17 @@
+
+
+Cible = {}
+ 
+
+  -- cible
+Cible = {}
+Cible.image = love.graphics.newImage("image/cibles/pumpkin.png")
+Cible.l = Cible.image:getWidth()
+Cible.h = Cible.image:getHeight()
+Cible.x = nil 
+Cible.y = nil
+Cible.vx = 100
+Cible.vy = 100
+Cible.mouv = false
+  
+
